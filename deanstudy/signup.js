fa00f42be9757f91bc16c9a4fc053d0207721981
Lee Dean <tlem312@gmail.com>
2016-04-18 23:57:51 +0900

    <noscript> 스크립트를 지원하지 않는 브라우저에서 표시되는 문구를 넣을 수 있음

--- deanstudy/signup.js
+++ deanstudy/signup.js
@@ -4,8 +4,11 @@
     <title>Example HTML Page </title>
   </head>
   <body>
-  <!--페이지 콘텐츠-->
-  <script type="text/javascript" src="example1.js"></script>
-  <script type="text/javascript" src="example2.js"></script>
+    <!--페이지 콘텐츠-->
+    <script type="text/javascript" src="example1.js"></script>
+    <script type="text/javascript" src="example2.js"></script>
+    <noscript>
+      <p>This page requires a Javascript-enabled browser.</p>
+    </noscript>
   </body>
 </html>
