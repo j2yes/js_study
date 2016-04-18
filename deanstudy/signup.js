17027670d1646b89ddb8b69481e8ceee3fa6bf4a
Lee Dean <tlem312@gmail.com>
2016-04-18 23:17:50 +0900

    자바스크립트 태그 위치 --> 페이지 랜더링 지연을 막기위해, 페이지 콘텐츠 하단부에 자바스크립트 코드를 붙입

--- deanstudy/signup.js
+++ deanstudy/signup.js
@@ -1,2 +1,11 @@
-dlkfjlsdjflkjs
-sldjflksjdlfjsdlfjlsk
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Example HTML Page </title>
+  </head>
+  <body>
+  <!--페이지 콘텐츠-->
+  <script type="text/javascript" src="example1.js"></script>
+  <script type="text/javascript" src="example2.js"></script>
+  </body>
+</html>
